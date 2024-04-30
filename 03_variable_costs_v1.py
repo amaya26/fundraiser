798f4df0f91ba9d50d83180aed43e61ce3b87861
amaya26 <165841780+amaya26@users.noreply.github.com>
2024-05-01 09:59:20 +1200

    Add files via upload

---
+++ 03_variable_costs_v1.py
@@ -0,0 +1,85 @@
+import pandas
+
+
+def num_check(question, error, num_type):
+    valid = False
+    while not valid:
+
+        try:
+            response = num_type(input(question))
+
+            if response <= 0:
+                print(error)
+            else:
+                return response
+
+        except ValueError:
+            print(error)
+
+
+def not_blank(question, error):
+
+    valid = False
+    while not valid:
+        response = input(question)
+
+        if response == "":
+            print("{}.  \nPlease try again.\n".format(error))
+            continue
+
+        return response
+
+
+# Currency formatting function
+def currency(x):
+
+
+# main routine starts here
+
+# set up dictionaries and lists
+
+item_list = []
+quantity_list = []
+price_list = []
+
+variable_dict = {
+    "Item": item_list,
+    "Quantity": quantity_list,
+    "Price": price_list
+}
+
+# Get user data
+product_name = not_blank("Product name: ",
+                         "The product name can't be blank. ")
+
+# Loop to get component, quantity and price
+item_name = ""
+while item_name.lower() != "xxx":
+
+    print()
+    # get name, quantity and item
+    item_name = not_blank("Item name: ",
+                          "The component name can't be"
+                          " blank. ")
+    if item_name.lower() == "xxx":
+        break
+
+    quantity = num_check("Quantity:",
+                         "The amount must be a whole number"
+                         " more than zero. ",
+                         int)
+    price = num_check("How much for a single item? $",
+                      "The price can't be a number <more"
+                      " than 0>",
+                      float)
+
+    # add item, quantity and price to lists
+    item_list.append(item_name)
+    quantity_list.append(quantity)
+    price_list.append(price)
+
+variable_frame = pandas.DataFrame(variable_dict)
+variable_frame = variable_frame.set_index('Item')
+
+# Calculate the cost of each component
+variable_frame["Cost"] = variable_frame
