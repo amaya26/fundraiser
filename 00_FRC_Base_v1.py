1a930b13c80f66602419a6f23b118ea72bcda9c3
amaya26 <165841780+amaya26@users.noreply.github.com>
2024-05-01 09:43:55 +1200

    Add files via upload

--- 00_FRC_Base_v1.py
+++ 00_FRC_Base_v1.py
@@ -21,6 +21,25 @@ def num_check(question, error, num_type):
             print(error)
 
 
+# Checks that user has entered ys / no to a question
+def yes_no(question):
+
+    to_check = ["yes", "no"]
+
+    valid = False
+    while not valid:
+
+        response = input(question).lower()
+
+        for var_item in to_check:
+            if response == var_item:
+                return response
+            elif response == var_item[0]:
+                return var_item
+
+        print("Please enter either yes or no...\n")
+
+
 # Main routine goes here
 get_int = num_check("How many do you need? ",
                     "Please enter an amount more than 0\n",
